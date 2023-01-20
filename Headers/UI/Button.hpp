658cd488edd99e09207b3558fae6fa2f6ca19ce4
Motisma479 <mat020930@gmail.com>
2023-01-20 03:50:08 +0100

    button working

--- Headers/UI/Button.hpp
+++ Headers/UI/Button.hpp
@@ -1,14 +1,31 @@
 #pragma once
+#include "Core/Maths/Maths.hpp"
+#include "Resources/Color.hpp"
+namespace Resources
+{
+	class Texture;
+}
+namespace Core
+{
+	struct Mouse;
+}
 
 namespace UI
 {
 	class Button
 	{
 	public:
-		Button();
+		Button(Resources::Texture& texture, Core::Maths::Vec2 pos, Core::Maths::Vec2 size, Resources::Color baseColor, Resources::Color hoveredColor);
 		~Button();
 
+		bool Update(Core::Mouse mouse);
 	private:
-
+		Resources::Texture& pTexture;
+		Core::Maths::Vec2 pPos;
+		Core::Maths::Vec2 pSize;
+		Resources::Color pBaseColor;
+		Resources::Color pHoveredColor;
+		Resources::Color curentColor;
+		bool isHovered(Core::Maths::Vec2 mousePos);
 	};
 }
--- Sources/UI/Button.cpp
+++ Sources/UI/Button.cpp
@@ -1,9 +1,36 @@
 #include "UI/Button.hpp"
-
-UI::Button::Button()
+#include "Core/Maths/Maths.hpp"
+#include "Resources/Texture.hpp"
+#include "Resources/Color.hpp"
+#include "Core/App.hpp"
+UI::Button::Button(Resources::Texture& texture, Core::Maths::Vec2 pos, Core::Maths::Vec2 size, Resources::Color baseColor, Resources::Color hoveredColor) 
+	: pTexture(texture), pPos(pos), pSize(size), pBaseColor(baseColor), pHoveredColor(hoveredColor)
 {
 }
 
 UI::Button::~Button()
 {
+}
+bool UI::Button::isHovered(Core::Maths::Vec2 mousePos)
+{
+	if (mousePos >= pPos - (Core::Maths::Vec2(pTexture.width(),pTexture.height())* pSize/2)&&
+		mousePos <= pPos + (Core::Maths::Vec2(pTexture.width(), pTexture.height()) * pSize / 2))
+		return true;
+	return false;
+}
+bool UI::Button::Update(Core::Mouse mouse)
+{
+	if (isHovered(mouse.position))
+	{
+		curentColor = pHoveredColor;
+		if (mouse.state[0] == 2)
+			return true;
+	}
+	else
+	{
+		curentColor = pBaseColor;
+	}
+	Resources::DrawTextureEx(pTexture, pPos, pSize, 0, curentColor);
+
+	return false;
 }
